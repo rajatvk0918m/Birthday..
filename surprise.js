95c9afb574087fd949220de332ceed56b333d532
rajatvk0918m <jaiswalnishant961@gmail.com>
2026-01-30 10:22:00 +0530

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,34 +1,30 @@
-const balloonContainer = document.getElementById("balloonContainer");
-const candle = document.getElementById("candle");
-const cake = document.getElementById("cake");
+let balloons = [
+  "balloon1.jpg",
+  "balloon2.jpg",
+  "balloon3.jpg",
+  "balloon4.jpg",
+  "balloon5.jpg"
+];
 
-let balloonCount = 0;
+let index = 0;
 
-function showNextBalloon(){
-    if(balloonCount < 5){
-        const balloon = document.createElement("div");
-        balloon.style.width = "60px";
-        balloon.style.height = "80px";
-        balloon.style.background = "blue";
-        balloon.style.borderRadius = "30px";
-        balloon.style.position = "absolute";
-        balloon.style.bottom = "0px";
-        balloon.style.left = `${10 + balloonCount*15}%`;
-        balloon.style.cursor = "pointer";
-        balloon.style.transition = "all 1s ease";
+function showBalloon(){
+  if(index < balloons.length){
+    let img = document.createElement("img");
+    img.src = balloons[index];
+    img.className = "balloon";
+    img.style.left = (10 + index*15) + "%";
 
-        balloon.onclick = () => {
-            balloon.style.transform = "translateY(-500px) rotate(360deg)";
-            balloon.style.opacity = 0;
-            balloonCount++;
-            setTimeout(showNextBalloon, 1000);
-        }
+    img.onclick = function(){
+      img.style.transform = "translateY(-600px)";
+      index++;
+      setTimeout(showBalloon,400);
+    };
 
-        balloonContainer.appendChild(balloon);
-    } else {
-        setTimeout(()=>{ candle.style.display="block"; }, 500);
-        setTimeout(()=>{ cake.style.display="block"; }, 2000);
-    }
+    document.body.appendChild(img);
+  }else{
+    document.getElementById("msg").style.display = "block";
+  }
 }
 
-showNextBalloon();
+showBalloon();
