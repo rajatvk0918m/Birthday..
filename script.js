cb8d2b3b8054956d5b162f67c7e8e868ff80b087
rajatvk0918m <jaiswalnishant961@gmail.com>
2026-01-30 10:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-const text = "𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂 🎉";
-let index = 0;
-const speed = 200;
+let text = "𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂";
+let i = 0;
+let typing = document.getElementById("typing");
+let btn = document.getElementById("surpriseBtn");
 
-const typingElement = document.getElementById("typing");
-const surpriseBtn = document.getElementById("surpriseBtn");
-
-function typeEffect() {
-    if(index < text.length){
-        typingElement.innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeEffect, speed);
-    } else {
-        // show surprise button after typing
-        setTimeout(() => {
-            surpriseBtn.style.opacity = 1;
-            surpriseBtn.style.pointerEvents = "auto";
-        }, 500);
-    }
+function typeEffect(){
+  if(i < text.length){
+    typing.innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect,150);
+  }else{
+    btn.style.display = "block";
+  }
 }
+
 typeEffect();
 
-function goToSurprise() {
-    document.getElementById("bgMusic").play();
-    window.location.href = "surprise.html";
-}
+btn.onclick = function(){
+  window.location.href = "surprise.html";
+};
