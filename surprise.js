f79b998862b78c9b766f8efa03d92f2ddd1ada44
rajatvk0918m <jaiswalnishant961@gmail.com>
2026-01-30 10:49:59 +0530

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,30 +1,11 @@
-let balloons = [
-  "balloon1.jpg",
-  "balloon2.jpg",
-  "balloon3.jpg",
-  "balloon4.jpg",
-  "balloon5.jpg"
-];
+let v = document.getElementById("vid");
+let opt = document.getElementById("options");
 
-let current = 0;
+v.onended = () => {
+  v.style.display = "none";
+  opt.style.display = "block";
+};
 
-function showNextBalloon(){
-  if(current < balloons.length){
-    let img = document.createElement("img");
-    img.src = balloons[current];
-    img.className = "balloon";
-    img.style.left = (10 + current * 15) + "%";
-
-    img.onclick = function(){
-      img.style.transform = "translateY(-700px)";
-      current++;
-      setTimeout(showNextBalloon, 500);
-    };
-
-    document.body.appendChild(img);
-  }else{
-    document.getElementById("cake").style.display = "block";
-  }
+function goCelebrate(){
+  window.location.href = "celebrate.html";
 }
-
-showNextBalloon();
