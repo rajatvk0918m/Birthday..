3ad26bdba2f0e692c692349ce2971ad32b9bb23d
rajatvk0918m <jaiswalnishant961@gmail.com>
2026-01-30 11:24:59 +0530

    Update surprise.js

--- surprise.js
+++ surprise.js
@@ -1,11 +1,12 @@
-let v = document.getElementById("vid");
-let opt = document.getElementById("options");
+let options = document.getElementById("options");
 
-v.onended = () => {
-  v.style.display = "none";
-  opt.style.display = "block";
-};
+// Delay 0.5s for small animation feel
+setTimeout(()=>{ options.style.display = "block"; }, 500);
 
 function goCelebrate(){
   window.location.href = "celebrate.html";
+}
+
+function goRecap(){
+  alert("Recap page coming soon!");
 }
