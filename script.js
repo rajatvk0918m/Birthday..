3d5757d05769a5618249ccd17777b58c24620ee1
rajatvk0918m <jaiswalnishant961@gmail.com>
2026-02-17 00:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let text="𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂";
-let i=0;
-let box=document.getElementById("birthdayText");
-let btn=document.getElementById("surpriseBtn");
+let text = "𝓗𝓪𝓹𝓹𝔂 𝓑𝓲𝓻𝓽𝓱𝓭𝓪𝔂";
+let i = 0;
+let box = document.getElementById("birthdayText");
+let btn = document.getElementById("surpriseBtn");
 
 function typeEffect(){
-  if(i<text.length){
-    box.innerHTML+=text.charAt(i);
+  if(i < text.length){
+    box.innerHTML += text.charAt(i);
     i++;
     setTimeout(typeEffect,150);
   }else{
-    btn.style.display="block";
+    btn.style.display = "block";
   }
 }
 
 typeEffect();
 
-btn.onclick=()=>{
-  window.location.href="surprise.html";
+btn.onclick = () => {
+  window.location.href = "surprise.html";
 };
